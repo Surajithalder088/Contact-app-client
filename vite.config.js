d5d9b4631372adc08bfbbf9da879f2c9e7cb2be3
Surajit <surajithalder088@gmail.com>
2024-03-09 19:01:54 +0530

    uri update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':"https://contact-app-server-a4im.onrender.com",
+      '/api':"https://contact-app-server-1.onrender.com",
     },
   },
   plugins: [react()],
